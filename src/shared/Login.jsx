cca15be931991172c9acdb3e5ca62b8440b54ba8
fojletuhin <fojletuhin@gmail.com>
2024-10-11 13:00:19 +0600

    add login page

--- src/shared/Login.jsx
+++ src/shared/Login.jsx
@@ -1,14 +1,8 @@
-
 const Login = () => {
     return (
-        <div className="w-[425px] min-h-screen bg-[#e6b325] mx-auto">
-            <img src="/public/onboard-1_zeprst.png" alt="" />
+     <div>
 
-            <div className="text-center">
-                <p>A Platform For Online Earners</p>
-                <p>Task Planet Is An Online Money Earning Website For The Individuals Looking For Small Tasks And Getting Paid For It</p>
-            </div>
-        </div>
+     </div>
     );
 };
 
